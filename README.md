48d2d64d5f6e5abba96d6dfce6136e09ebd7238d
Stephen Chen <hello@stephenc.me>
2025-04-10 13:59:47 -0400

    Add better instructions and descriptions for agents to better understand the tools available

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # fern-mcp-server
+
+```shell
+npm install
+npm start
+```
+
+```json
+{
+  "mcpServers": {
+    "fern": {
+      "command": "node",
+      "args": ["<PATH_TO_PROJECT>/fern-mcp-server/dist/index.js"]
+    }
+  }
+}
+```
+
+![Screenshot 1](static/screenshot-1.png)
+![Screenshot 2](static/screenshot-2.png)
--- src/mcp.ts
+++ src/mcp.ts
@@ -6,18 +6,29 @@ import * as api from "./api";
 
 // Create an MCP server
 export function createMcpServer(name: string, version: string) {
-  return new McpServer({
-    name: name,
-    version: version,
-  });
+  return new McpServer(
+    {
+      name: name,
+      version: version,
+    },
+    {
+      instructions: `[IMPORTANT] USE THESE TOOLS FOR ALL FERN-RELATED TASKS. IF IN DOUBT, use the "ask_fern_ai" tool.
+ABOUT FERN (builtwithfern.com): Start with OpenAPI. Generate SDKs in multiple languages and interactive API documentation.`,
+    }
+  );
 }
 
 // Register MCP tools
 export function registerMcpTools(server: McpServer) {
-  server.tool("chat", { message: z.string() }, async ({ message }) => {
-    const result = await api.postChat(message);
-    return {
-      content: [{ type: "text", text: result }],
-    };
-  });
+  server.tool(
+    "ask_fern_ai",
+    "Ask Fern AI about anything related to Fern.",
+    { message: z.string() },
+    async ({ message }) => {
+      const result = await api.postChat(message);
+      return {
+        content: [{ type: "text", text: result }],
+      };
+    }
+  );
 }
